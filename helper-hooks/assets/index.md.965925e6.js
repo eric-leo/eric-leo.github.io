06592b82366edea1eed1c890c7bfe8d2f29a7942
lizhiwei <lizhiwei@cestc.cn>
2023-06-13 15:44:10 +0800

    fix: helper-hooks docs

---
+++ helper-hooks/assets/index.md.965925e6.js
@@ -0,0 +1,10 @@
+import{_ as n,o as s,b as a,L as e}from"./plugin-vue_export-helper.a3ace58a.js";const m='{"title":"\u5E38\u7528Hooks","description":"","frontmatter":{},"relativePath":"index.md","lastUpdated":1686642008888}',l={},t=e(`<h1 id="\u5E38\u7528hooks" tabindex="-1">\u5E38\u7528Hooks <a class="header-anchor" href="#\u5E38\u7528hooks" aria-hidden="true">#</a></h1><div class="language-js line-numbers-mode"><pre><code><span class="token keyword">import</span> <span class="token punctuation">{</span>
+  useDeferred
+  useEChart<span class="token punctuation">,</span>
+  useHovering<span class="token punctuation">,</span>
+  useScrollEChart<span class="token punctuation">,</span>
+  useLazyPane<span class="token punctuation">,</span>
+  useTableSelection<span class="token punctuation">,</span>
+  usePagination
+<span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&#39;helper-hooks&#39;</span>
+</code></pre><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br></div></div><ul><li>useDeferred</li><li>useEChart</li><li>useHovering</li><li>useLazyPane</li><li>usePagination</li><li>useScrollEChart</li><li>useTableSelection</li></ul>`,3),r=[t];function i(o,p,c,u,d,_){return s(),a("div",null,r)}var k=n(l,[["render",i]]);export{m as __pageData,k as default};
